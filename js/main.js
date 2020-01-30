b154bee02e73152d8411f0c7cf91b3171fd012d3
Айдан Татышева <aidan@Ajdans-MacBook-Pro.local>
2020-01-31 00:12:06 +0600

    includes

--- js/main.js
+++ js/main.js
@@ -6,3 +6,31 @@ $(document).ready(function(){
       autoplayTimeout: 3000
     });
   });
+
+//   const { readFileSync } = require('fs')
+
+// const posthtml = require('posthtml')
+// const include = require('include')
+
+// const html = readFileSync('index.html')
+
+// posthtml([ include({ encoding: 'utf8' }) ])
+//     .process(html)
+//     .then((result) => console.log(result.html))
+
+//     window.onscroll = function() {myFunction()};
+
+// // Get the header
+// var header = document.getElementById("myHeader");
+
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
